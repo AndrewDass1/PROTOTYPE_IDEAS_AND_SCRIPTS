dd4a668cc4b496f29b74d08cfb4896ef9ab87c9e
Andrew Dass <dassandrew3@gmail.com>
2022-09-15 21:53:47 -0400

    Add files via upload

---
+++ notification_reminder.py
@@ -0,0 +1,7 @@
+import time
+
+while 1 > 0:
+    time.sleep(1200)
+    print("20 minutes has passed! 20-20-20 rule time!" + "\n" + "For the next 20 seconds, look away from this device! Blink and look at another object 20 feet away from you for 20 seconds!")
+    time.sleep(20)
+    print("Back to work!")
