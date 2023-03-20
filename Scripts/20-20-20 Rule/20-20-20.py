632032d1d2cfb59cffd4f654cdc117169e2cd56f
Andrew Dass <dassandrew3@gmail.com>
2023-03-19 20:42:33 -0400

    Create 20-20-20.py

---
+++ Scripts/20-20-20 Rule/20-20-20.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+import time
+
+time.sleep(1200)
+os.system("echo 20 minutes has passed. Follow the 20-20-20 rule.")
+os.system("echo Look away from the screen for 20 seconds and look at an object 20 feet away!")
+
+time.sleep(20)
+os.system("echo 20 seconds passed. Back to work!")
