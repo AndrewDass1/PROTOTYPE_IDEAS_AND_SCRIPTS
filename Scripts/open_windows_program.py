e85b3a314f0295da6fc09482813349f16ccdbf22
Andrew Dass <dassandrew3@gmail.com>
2023-05-29 19:23:23 -0400

    Create open_windows_program.py

---
+++ Scripts/open_windows_program.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env Python3
+# This is a script that can be automated to open any program on startup if specified. This script uses notepad as an example to showcase this. 
+
+import os
+
+os.system('notepad')
