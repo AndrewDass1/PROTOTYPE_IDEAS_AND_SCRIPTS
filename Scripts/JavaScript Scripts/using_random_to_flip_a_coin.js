4e63150af48d0c51a2bb81cec05e39565d0f6a79
Andrew Dass <dassandrew3@gmail.com>
2024-03-18 19:55:45 -0400

    Create using_random_to_flip_a_coin.js
    In JavaScript, I wrote code that utilizes Math.random() to give a number between 1 - 1.99. If the number is 1.50 or greater, then the Math.round() is used on the number that is outputted from Math.random. An if and else statement is then used to print out, if the number is greater than or equal to 1.50, then it is Heads, otherwise, it is Tails.

---
+++ Scripts/JavaScript Scripts/using_random_to_flip_a_coin.js
@@ -0,0 +1,13 @@
+random_number = (Math.random()) + 1;
+
+round_the_random_number = Math.round(random_number);
+
+// console.log(random_number)
+// console.log(round_the_random_number)
+
+if (round_the_random_number >= 1.50) {
+    console.log("Heads");
+}
+else {
+    console.log("Tails");
+}
