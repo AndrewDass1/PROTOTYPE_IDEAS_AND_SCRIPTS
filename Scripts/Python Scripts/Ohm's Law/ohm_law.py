61579aa967c4601343a2ef8011ebff07efe2e1a2
Andrew Dass <dassandrew3@gmail.com>
2024-03-23 18:56:06 -0400

    Create ohm_law.py
    While reviewing more Electrical Engineering or Electrical terms, I wanted to write a short Python script, that uses Ohm's law. It asks for a current and resistance value, and both are multiplied to calculate the voltage.

---
+++ Scripts/Python Scripts/Ohm's Law/ohm_law.py
@@ -0,0 +1,7 @@
+ask_for_current = float(input("Enter current: "))
+
+ask_for_resistance = float(input("Enter resistance: "))
+
+calculate_voltage = ask_for_current * ask_for_resistance
+
+print(calculate_voltage)
